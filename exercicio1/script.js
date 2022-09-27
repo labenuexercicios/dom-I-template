254c37ebfd573647c2ab09a2bb61f26f70c8ae42
Chay-May <chayenne.mayara@gmail.com>
2022-09-26 21:05:26 -0300

    Dom - aula on 027

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+let frase = document.getElementById('sapo')
+console.log(frase)
+
+frase.innerHTML += " pé, não lava porque não quer."
+
+let frase2 = document.getElementById('poema')
+
+frase2.innerHTML += " evidencias, mas pra que viver fingindo, se eu não posso enganar meu coração... só sei que te amo"
+
+console.log(frase2)
+
+let frase3 = document.getElementById('sabedoria')
+
+frase3.innerHTML += " voando"
+console.log(frase3)
+
+
+let frase4 = document. getElementById('borboleta')
+
+frase4.innerHTML += " pica-pau"
+console.log (frase4)
+
+let frase5 = document.getElementById('roberto')
+
+frase5.innerHTML += " vivi..."
+console.log(frase5)
---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+let input = document.getElementById('nome')
+console.log(input.value)
+
+input.value = ""
+
+let input2 = document.getElementById('endereco')
+
+input2.value = ""
+console.log(input2.value)
+
+let input3 = document.getElementById('email')
+
+input3.value = ""
+console.log(input3.value)
+

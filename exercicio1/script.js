57402db4c4444efd3502e7bbb10293c30962eff5
Joanadayse <daysejoana3@gmail.com>
2022-09-27 13:37:06 -0300

    pratica-guiada

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+ let frase = document.getElementById("sapo")
+
+ frase.innerHTML += ' o pé  '
+
+ let frase1 = document.getElementById("poema")
+ frase1.innerHTML += ' evidencias' 
+
+ let frase2= document.getElementById("sabedoria")
+ frase2.innerHTML += ' do que dois voando' 
+
+ let frase3 = document.getElementById("borboleta")
+ frase3.innerHTML += ' pica-pau '
+
+ let frase4 = document.getElementById("roberto")
+ frase4.innerHTML += ' vivi'
---
+++ fixacao/script.js
@@ -0,0 +1,7 @@
+let frase = document.getElementById('paragrafo')
+let input = document.getElementById('texto')
+
+function nomeUsuario(){
+  frase.innerHTML += input.value
+
+}

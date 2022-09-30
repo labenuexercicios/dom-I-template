cfd192e18316cb521081afae89d2165adf8e7a1f
Chay-May <chayenne.mayara@gmail.com>
2022-09-29 21:06:54 -0300

    Exercício DOM

--- fixacao/script.js
+++ fixacao/script.js
@@ -3,6 +3,6 @@ let frase = document.getElementById('paragrafo')
 let input = document.getElementById('texto')
 
 function nomeUsuario() {
-    frase.innerHTML = frase.innerHTML+" "+input.value // Concatenação de espaço na frase
+    frase.innerHTML = frase.innerHTML+" "+input.value // Concatenação de espaço 
     // console.log(`Olá, ${frase}!`)
 }

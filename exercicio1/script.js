9ed70e18c62e4f42a237444842570d4df48105cc
bruna23 <bruna.vf23@gmail.com>
2022-10-03 11:30:55 -0300

    pratica-guiada a027 dom

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+let fraseSapo = document.getElementById("sapo")
+fraseSapo.innerHTML = fraseSapo.innerHTML + " pé, não lava porque não quer"
+
+let frasePoema = document.getElementById("poema")
+frasePoema.innerHTML = frasePoema.innerHTML + " evidências"
+
+let fraseSabedoria = document.getElementById("sabedoria")
+fraseSabedoria.innerHTML = fraseSabedoria.innerHTML + " voando"
+
+let fraseBorboleta = document.getElementById("borboleta")
+fraseBorboleta.innerHTML = fraseBorboleta.innerHTML + " pica-pau"
+
+let fraseRoberto = document.getElementById("roberto")
+fraseRoberto.innerHTML = fraseRoberto.innerHTML + " vivi"

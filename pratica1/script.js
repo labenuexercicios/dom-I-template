7ac7e9b43ddb4b87f367974d471834ad1f031ff7
Thais Contecote Christofalo <thaiscc@MacBook-Air-de-Thais.local>
2023-11-13 10:39:16 -0300

    domI

---
+++ pratica1/script.js
@@ -0,0 +1,15 @@
+const sapo = document.getElementById("sapo")
+sapo.innerHTML += " pé"
+console.log()
+
+const poema = document.getElementById("poema")
+poema.innerHTML += " evidencias"
+
+const sabedoria = document.getElementById("sabedoria")
+sabedoria.innerHTML += " voando"
+
+const borboleta = document.getElementById("borboleta")
+borboleta.innerHTML += " pica-pau"
+
+const roberto = document.getElementById("roberto")
+roberto.innerHTML += " vivi"
---
+++ pratica3/script.js
@@ -0,0 +1,8 @@
+let contador = 0
+
+console.log("valor inicial", contador)
+const contarClicar = () => {
+    contador += 1
+    console.log("cliques:", contador)
+
+}
